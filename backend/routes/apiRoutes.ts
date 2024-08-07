3a7d84e1c6161a18c5ceae9a49f9f73b190e7f66
JoelBenton <joelbenton1903@gmail.com>
2024-08-07 22:00:52 +0100

    Added new line

--- backend/routes/apiRoutes.ts
+++ backend/routes/apiRoutes.ts
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 const apiController = require('../controllers/apiController');
 
+
 // Define your routes
 router.post('/mangaRecommendations', apiController.mangaRecommendations);
 
